0315165f0a5c73355edb312a501724d027d19949
nanaperez <nanaperez01@gmail.com>
2021-12-02 17:41:43 -0500

    Segunda version con modelos, regresion logistica, sigue el cliente y luego el servidor

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,13 @@
 import numpy as np
+import pandas as pd
 from typing import Tuple, Union, List
+from sklearn.model_selection import train_test_split
 
 XY = Tuple[np.ndarray, np.ndarray]
 Dataset = Tuple[XY, XY]
 LogRegParams = Union[XY, Tuple[np.ndarray]]
 XYList = List[XY]
+
 
 def revolver(X: np.ndarray, y: np.ndarray) -> XY:
     """Shuffle X and y."""
@@ -12,20 +15,21 @@ def revolver(X: np.ndarray, y: np.ndarray) -> XY:
     idx = rng.permutation(len(X))
     return X[idx], y[idx]
 
+
 def particionar(X: np.ndarray, y: np.ndarray, num_partitions: int) -> XYList:
     """Split X and y into a number of partitions."""
     return list(
-        zip(np.array_split(X, num_partitions), np.array_split(y, num_partitions))
+        zip(np.array_split(X, num_partitions),
+            np.array_split(y, num_partitions))
     )
 
-def cargar_datos(nombre) -> Dataset:
+
+def cargar_datos(ruta_archivo: str, div_ratio=0.8) -> Dataset:
     """Cargar los datos de la ruta de procesado
     """
-    mnist_openml = openml.datasets.get_dataset(554)
-    Xy, _, _, _ = mnist_openml.get_data(dataset_format="array")
-    X = Xy[:, :-1]  # the last column contains labels
-    y = Xy[:, -1]
-    # First 60000 samples consist of the train set
-    x_train, y_train = X[:60000], y[:60000]
-    x_test, y_test = X[60000:], y[60000:]
+    df_datos = pd.read_csv(ruta_archivo)
+    X = df_datos.iloc[:, :-1]  # the last column contains labels
+    y = df_datos.iloc[:, -1]
+    x_train, x_test, y_train, y_test = train_test_split(
+        X, y, train_size=div_ratio)
     return (x_train, y_train), (x_test, y_test)
